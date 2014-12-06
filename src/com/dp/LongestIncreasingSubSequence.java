c59cbf9dae723dd40ae0276d843f6c39b5d3938f
Octo <sayed.atif.ali@gmail.com>
2014-12-06 13:21:52 +0530

    Added O(N*log(N)) solution for finding longest increasing subsequence

--- src/com/dp/LongestIncreasingSubSequence.java
+++ src/com/dp/LongestIncreasingSubSequence.java
@@ -33,16 +33,18 @@ public class LongestIncreasingSubSequence {
 
 		for (i = 0; i < aList.size(); i++)
 			a[i] = aList.get(i);
-		
+
 		max = lis(a);
-		
+
 		System.out.println("Input list: ");
 		for (i = 0; i < a.length; i++)
 			System.out.print(i + ": " + a[i] + ", ");
-		
+
 		System.out.println();
-		
+
 		System.out.println("LIS of input sequence: " + max);
+		max = lisNLogN(a);
+		System.out.println("LIS of input sequence using NLogN method: " + max);
 	}
 
 	public static int lis(int[] val) {
@@ -67,8 +69,8 @@ public class LongestIncreasingSubSequence {
 			indexes[i] = i;
 		}
 
-		//Find and store the maximum LIS ending at index i
-		
+		// Find and store the maximum LIS ending at index i
+
 		for (i = 1; i < val.length; i++) {
 
 			max = val[i];
@@ -81,7 +83,7 @@ public class LongestIncreasingSubSequence {
 
 			}
 		}
-		
+
 		max = 0;
 		j = 0;
 		for (i = 0; i < dp.length; i++) {
@@ -91,17 +93,67 @@ public class LongestIncreasingSubSequence {
 			max = Math.max(max, dp[i]);
 		}
 		System.out.println();
-		
+
 		for (i = 0; i < indexes.length; i++) {
 			System.out.print(i + ": " + indexes[i] + ", ");
 		}
-//		printSolution(indexes, j);
+		// printSolution(indexes, j);
 		System.out.println();
-		
+
 		return max;
 
 	}
-	
+
+	// LIS O(NLogN) solution
+
+	public static int lisNLogN(int[] array) {
+
+		int N;
+		int l;
+		int max = 0;
+		ArrayList<Long> ans;
+
+		ans = new ArrayList<Long>();
+		N = array.length;
+
+		for (int i = 0; i < N; i++) {
+
+			l = lowerBound(ans, 0, ans.size() - 1, array[i]);
+
+			if (l == ans.size()) {
+				ans.add(array[i] * 1L);
+			} else {
+				ans.set(l, array[i] * 1L);
+			}
+		}
+
+		max = ans.size();
+		return max;
+	}
+
+	public static int lowerBound(ArrayList<Long> array, int first, int last,
+			long val) {
+
+		int count, step, it;
+
+		count = last - first + 1;
+
+		while (count > 0) {
+			it = first;
+			step = count / 2;
+			it += step;
+
+			if (array.get(it) < val) {
+				first = ++it;
+				count -= step + 1;
+			} else {
+				count = step;
+			}
+		}
+
+		return first;
+	}
+
 	public static void printSolution(int[] index, int i) {
 
 		if (index[i] == 1) {
